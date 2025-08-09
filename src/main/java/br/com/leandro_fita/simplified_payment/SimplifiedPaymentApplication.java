3c572356a019dd00ccc9b5f02d1b0917d2da5838
Leandro Fita <leandrocfita@gmail.com>
2025-08-09 17:33:33 -0300

    feat: Files that weren't being versioned
    Adding files that weren't being versioned
    
    Configuring database and database related configs

---
+++ src/main/java/br/com/leandro_fita/simplified_payment/SimplifiedPaymentApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandro_fita.simplified_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandro_fita/simplified_payment/SimplifiedPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandro_fita.simplified_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
